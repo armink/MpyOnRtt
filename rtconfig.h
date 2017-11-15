6697d9a6bf00abc47ced2d2c5a697644f254ad5c
armink <armink.ztl@gmail.com>
2017-11-15 22:05:23 +0800

    1、【增加】部分 Kconfig 文件，保证 Menuconfig 得以正常启动。
    Signed-off-by: armink <armink.ztl@gmail.com>

--- rtconfig.h
+++ rtconfig.h
@@ -1,144 +1,196 @@
-/* RT-Thread config file */
-#ifndef __RTTHREAD_CFG_H__
-#define __RTTHREAD_CFG_H__
+#ifndef RT_CONFIG_H__
+#define RT_CONFIG_H__
 
-/* RT_NAME_MAX*/
-#define RT_NAME_MAX	24
+/* Automatically generated file; DO NOT EDIT. */
+/* RT-Thread Configuration */
 
-/* RT_ALIGN_SIZE*/
-#define RT_ALIGN_SIZE	4
+/* RT-Thread Kernel */
 
-/* PRIORITY_MAX */
-#define RT_THREAD_PRIORITY_MAX	32
-
-/* Tick per Second */
-#define RT_TICK_PER_SECOND	    1000
-
-/* SECTION: RT_DEBUG */
-/* Thread Debug */
+#define RT_NAME_MAX 24
+#define RT_ALIGN_SIZE 4
+#define RT_THREAD_PRIORITY_MAX 32
+#define RT_TICK_PER_SECOND 1000
 #define RT_DEBUG
-#define RT_THREAD_DEBUG
-
 #define RT_USING_OVERFLOW_CHECK
-
-/* Using Hook */
+#define RT_DEBUG_INIT 0
+/* RT_DEBUG_THREAD is not set */
 #define RT_USING_HOOK
-
-/* Using Software Timer */
+#define IDLE_THREAD_STACK_SIZE 256
 #define RT_USING_TIMER_SOFT
-#define RT_TIMER_THREAD_PRIO		4
-#define RT_TIMER_THREAD_STACK_SIZE	512
-#define RT_TIMER_TICK_PER_SECOND	RT_TICK_PER_SECOND
+#define RT_TIMER_THREAD_PRIO 4
+#define RT_TIMER_THREAD_STACK_SIZE 512
 
-/* SECTION: IPC */
-/* Using Semaphore*/
+/* Inter-Thread communication */
+
 #define RT_USING_SEMAPHORE
-
-/* Using Mutex */
 #define RT_USING_MUTEX
-
-/* Using Event */
 #define RT_USING_EVENT
-
-/* Using MailBox */
 #define RT_USING_MAILBOX
-
-/* Using Message Queue */
 #define RT_USING_MESSAGEQUEUE
+/* RT_USING_SIGNALS is not set */
 
-/* SECTION: Memory Management */
-/* Using Memory Pool Management*/
+/* Memory Management */
+
 #define RT_USING_MEMPOOL
-
-/* Using Dynamic Heap Management */
+/* RT_USING_MEMHEAP is not set */
 #define RT_USING_HEAP
-
-/* Using Small MM */
 #define RT_USING_SMALL_MEM
+/* RT_USING_SLAB is not set */
 
-// <bool name="RT_USING_COMPONENTS_INIT" description="Using RT-Thread components initialization" default="true" />
-#define RT_USING_COMPONENTS_INIT
+/* Kernel Device Object */
 
-/* SECTION: Device System */
-/* Using Device System */
 #define RT_USING_DEVICE
-// <bool name="RT_USING_DEVICE_IPC" description="Using device communication" default="true" />
-#define RT_USING_DEVICE_IPC
-// <bool name="RT_USING_SERIAL" description="Using Serial" default="true" />
-#define RT_USING_SERIAL
-#define RT_SERIAL_RB_BUFSZ 1024
-// <bool name="RT_USING_RTC" description="Using RTC" default="true" />
-#define RT_USING_RTC
-// <bool name="RT_USING_PIN" description="Using generic GPIO device drivers" default="true" />
-#define RT_USING_PIN
-// <bool name="RT_USING_SPI" description="Using SPI Device Driver Framework" default="true" />
-#define RT_USING_SPI
-
-
-/* SECTION: Console options */
 #define RT_USING_CONSOLE
-/* the buffer size of console*/
-#define RT_CONSOLEBUF_SIZE          1024
-// <string name="RT_CONSOLE_DEVICE_NAME" description="The device name for console" default="uart1" />
-#define RT_CONSOLE_DEVICE_NAME      "uart1"
+#define RT_CONSOLEBUF_SIZE 1024
+#define RT_CONSOLE_DEVICE_NAME "uart1"
+/* RT_USING_MODULE is not set */
+#define ARCH_ARM
+#define ARCH_ARM_CORTEX_M
+#define ARCH_ARM_CORTEX_M4
 
-/* SECTION: finsh, a C-Express shell */
+/* RT-Thread Components */
+
+#define RT_USING_COMPONENTS_INIT
+#define RT_USING_USER_MAIN
+
+/* C++ features */
+
+/* RT_USING_CPLUSPLUS is not set */
+
+/* Command shell */
+
 #define RT_USING_FINSH
-/* Using symbol table */
+#define FINSH_USING_HISTORY
 #define FINSH_USING_SYMTAB
 #define FINSH_USING_DESCRIPTION
-/* Using msh and finsh */
-#define FINSH_USING_MSH
-/* Shell default using MSH mode */
-#define FINSH_USING_MSH_DEFAULT
-/* command line max length */
-#define FINSH_CMD_SIZE 256
-/* finsh thread stack size */
+#define FINSH_THREAD_PRIORITY 20
 #define FINSH_THREAD_STACK_SIZE 4096
-/* Only enabled when firmware is released */
-#ifdef RELEASE
-/* Using password authentication */
-#define FINSH_USING_AUTH
-#define FINSH_DEFAULT_PASSWORD "61866139"
-#endif /* RELEASE */
+#define FINSH_CMD_SIZE 256
+/* FINSH_USING_AUTH is not set */
+#define FINSH_USING_MSH
+#define FINSH_USING_MSH_DEFAULT
+/* FINSH_USING_MSH_ONLY is not set */
 
-/* SECTION: device filesystem */
+/* Device virtual file system */
+
 #define RT_USING_DFS
-#define RT_USING_DFS_DEVFS
-/* support fatfs */
-#define RT_USING_DFS_ELMFAT
-/* make sure only one thread can WR in one time, fix reentrant problem */
-#define RT_DFS_ELM_REENTRANT
-#define RT_DFS_ELM_WORD_ACCESS
-/* using working directory */
 #define DFS_USING_WORKDIR
-/* number of volumes to be used */
-#define RT_DFS_ELM_DRIVES           2
-/* long file name.
-    mode 1: use static buffer, may cause reentry problem
-    mode 2: use stack, need large stack
-    mode 3: use heap, the most safe one */
-#define RT_DFS_ELM_USE_LFN          3
-#define RT_DFS_ELM_MAX_LFN          255
-#define RT_DFS_ELM_CODE_PAGE        437
-/* need meet flash data sheet spec. One page = 4096 Bytes */
-#define RT_DFS_ELM_MAX_SECTOR_SIZE          4096
-/* the max number of mounted filesystem */
-#define DFS_FILESYSTEMS_MAX         2
-/* the max number of opened files       */
-#define DFS_FD_MAX              4
+#define DFS_FILESYSTEMS_MAX 2
+#define DFS_FD_MAX 4
+#define RT_USING_DFS_ELMFAT
 
-/* SECTION: Serial Flash Universal Driver */
+/* elm-chan's FatFs, Generic FAT Filesystem Module */
+
+#define RT_DFS_ELM_CODE_PAGE 437
+#define RT_DFS_ELM_WORD_ACCESS
+#define RT_DFS_ELM_USE_LFN_0
+/* RT_DFS_ELM_USE_LFN_1 is not set */
+/* RT_DFS_ELM_USE_LFN_2 is not set */
+/* RT_DFS_ELM_USE_LFN_3 is not set */
+#define RT_DFS_ELM_USE_LFN 0
+#define RT_DFS_ELM_MAX_LFN 255
+#define RT_DFS_ELM_DRIVES 2
+#define RT_DFS_ELM_MAX_SECTOR_SIZE 4096
+/* RT_DFS_ELM_USE_ERASE is not set */
+#define RT_DFS_ELM_REENTRANT
+#define RT_USING_DFS_DEVFS
+/* RT_USING_DFS_NET is not set */
+/* RT_USING_DFS_ROMFS is not set */
+/* RT_USING_DFS_RAMFS is not set */
+/* RT_USING_DFS_UFFS is not set */
+
+/* Device Drivers */
+
+#define RT_USING_DEVICE_IPC
+#define RT_USING_SERIAL
+/* RT_USING_CAN is not set */
+/* RT_USING_HWTIMER is not set */
+/* RT_USING_I2C is not set */
+#define RT_USING_PIN
+/* RT_USING_MTD_NOR is not set */
+/* RT_USING_MTD_NAND is not set */
+#define RT_USING_RTC
+/* RT_USING_SDIO is not set */
+#define RT_USING_SPI
 #define RT_USING_SFUD
-//#define RT_SFUD_DEBUG
 #define RT_SFUD_USING_SFDP
-//#define RT_SFUD_USING_FLASH_INFO_TABLE
+#define RT_SFUD_USING_FLASH_INFO_TABLE
+/* RT_SFUD_DEBUG is not set */
+/* RT_USING_W25QXX is not set */
+/* RT_USING_GD is not set */
+/* RT_USING_ENC28J60 is not set */
+/* RT_USING_SPI_WIFI is not set */
+/* RT_USING_WDT is not set */
+/* RT_USING_USB_HOST is not set */
+/* RT_USING_USB_DEVICE is not set */
 
-/* SECTION: C Runtime library setting */
-// <bool name="RT_USING_LIBC" description="Using C library" default="true" />
+/* POSIX layer and C standard library */
+
 #define RT_USING_LIBC
+/* RT_USING_PTHREADS is not set */
 #define RT_USING_POSIX
-// <bool name="RT_USING_PTHREADS" description="Using POSIX threads library" default="true" />
-//#define RT_USING_PTHREADS
+/* RT_USING_POSIX_MMAP is not set */
+/* RT_USING_POSIX_TERMIOS is not set */
+
+/* Network stack */
+
+/* light weight TCP/IP stack */
+
+/* RT_USING_LWIP is not set */
+
+/* Modbus master and slave stack */
+
+/* RT_USING_MODBUS is not set */
+
+/* RT-Thread UI Engine */
+
+/* RT_USING_GUIENGINE is not set */
+
+/* VBUS(Virtual Software BUS) */
+
+/* RT_USING_VBUS is not set */
+
+/* RT-Thread online packages */
+
+/* system packages */
+
+/* PKG_USING_PARTITION is not set */
+/* PKG_USING_SQLITE is not set */
+
+/* IoT - internet of things */
+
+/* PKG_USING_PAHOMQTT is not set */
+/* PKG_USING_WEBCLIENT is not set */
+/* PKG_USING_MONGOOSE is not set */
+/* PKG_USING_WEBTERMINAL is not set */
+/* PKG_USING_CJSON is not set */
+/* PKG_USING_EZXML is not set */
+
+/* security packages */
+
+/* PKG_USING_MBEDTLS is not set */
+
+/* language packages */
+
+/* PKG_USING_JERRYSCRIPT is not set */
+
+/* multimedia packages */
+
+/* PKG_USING_FASTLZ is not set */
+
+/* tools packages */
+
+/* PKG_USING_CMBACKTRACE is not set */
+/* PKG_USING_EASYLOGGER is not set */
+/* PKG_USING_SYSTEMVIEW is not set */
+
+/* miscellaneous packages */
+
+/* PKG_USING_HELLO is not set */
+#define BOARD_ART_6LOWPAN
+#define RT_USING_UART1
+/* RT_USING_UART2 is not set */
+#define RT_USING_UART3
+/* RT_USING_SPI1 is not set */
 
 #endif
